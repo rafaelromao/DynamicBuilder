af7115f9712d70cca655d394ad50ea516ecc7a5c
Rafael Romão <rafaelromao@gmail.com>
2013-03-14 08:06:42 -0300

    Playground project created

---
+++ DynamicBuilder.Playground/Program.cs
@@ -0,0 +1,39 @@
+﻿using DynamicBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground
+{
+    class Program
+    {
+        class SomeClass
+        {
+            public string SomeProperty { get; set; }
+            public DateTime SomeOtherProperty { get; set; }
+
+            public override string ToString()
+            {
+                return SomeProperty + SomeOtherProperty.ToShortDateString();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var factory = new BuilderFactory();
+            dynamic someClassBuilder = factory.ABuilderFor<SomeClass>();
+            
+            SomeClass someClass1 = someClassBuilder.WithSomeProperty("Today is ")
+                                                   .WithSomeOtherProperty(DateTime.Today)
+                                                   .Value;
+            SomeClass someClass2 = someClassBuilder.WithSomeProperty("Yesterday was ")
+                                                   .WithSomeOtherProperty(DateTime.Today.AddDays(-1))
+                                                   .Value;
+            Console.WriteLine(someClass1);
+            Console.WriteLine(someClass2);
+            Console.ReadKey();
+        }
+    }
+}
