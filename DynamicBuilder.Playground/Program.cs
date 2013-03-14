0ffff19794a1f0af34140ee03d77ebf4ad7a912a
Rafael Romão <rafaelromao@gmail.com>
2013-03-14 08:11:59 -0300

    Playground sample updated

--- DynamicBuilder.Playground/Program.cs
+++ DynamicBuilder.Playground/Program.cs
@@ -9,30 +9,30 @@ namespace Playground
 {
     class Program
     {
-        class SomeClass
+        class HelloWorld
         {
-            public string SomeProperty { get; set; }
-            public DateTime SomeOtherProperty { get; set; }
+            public string Message { get; set; }
+            public DateTime Date { get; set; }
 
             public override string ToString()
             {
-                return SomeProperty + SomeOtherProperty.ToShortDateString();
+                return String.Format(Message, Date.ToShortDateString());
             }
         }
 
         static void Main(string[] args)
         {
             var factory = new BuilderFactory();
-            dynamic someClassBuilder = factory.ABuilderFor<SomeClass>();
+            dynamic helloWorldBuilder = factory.ABuilderFor<HelloWorld>();
             
-            SomeClass someClass1 = someClassBuilder.WithSomeProperty("Today is ")
-                                                   .WithSomeOtherProperty(DateTime.Today)
-                                                   .Value;
-            SomeClass someClass2 = someClassBuilder.WithSomeProperty("Yesterday was ")
-                                                   .WithSomeOtherProperty(DateTime.Today.AddDays(-1))
-                                                   .Value;
-            Console.WriteLine(someClass1);
-            Console.WriteLine(someClass2);
+            HelloWorld helloWorld1 = helloWorldBuilder.WithMessage("Today is {0}")
+                                                      .WithDate(DateTime.Today)
+                                                      .Value;
+            HelloWorld helloWorld2 = helloWorldBuilder.WithMessage("Yesterday was {0}")
+                                                      .WithDate(DateTime.Today.AddDays(-1))
+                                                      .Value;
+            Console.WriteLine(helloWorld1);
+            Console.WriteLine(helloWorld2);
             Console.ReadKey();
         }
     }
